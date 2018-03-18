9517c35cf9526a705626d212047e0ff7032a552f
yaronw <yaronw@perfectomobile.com>
2018-03-18 17:45:00 +0200

    pom

--- src/main/java/com/quantum/steps/POMStepDefs.java
+++ src/main/java/com/quantum/steps/POMStepDefs.java
@@ -15,20 +15,6 @@ import static com.quantum.listerners.QuantumReportiumListener.logStepStart;
 public class POMStepDefs extends WebDriverTestCase {
     private final String USERNAME = getBundle().getPropertyValue("username");
     private final String PASSWORD = getBundle().getPropertyValue("password");
-    private final String UNIT = getBundle().getPropertyValue("unit");
-    private final String OWNER_FIRSTNAME = getBundle().getPropertyValue("ownerFirstName");
-    private final String OWNER_LASTNAME = getBundle().getPropertyValue("ownerLastName");
-    private final String COMPANY = getBundle().getPropertyValue("company");
-    private final String MONTH = getBundle().getPropertyValue("month");
-    private final String DAY = getBundle().getPropertyValue("day");
-    private final String YEAR = getBundle().getPropertyValue("year");
-    private final String EMAIL = getBundle().getPropertyValue("email");
-    private final String COUNTRY = getBundle().getPropertyValue("country");
-    private final String CITY = getBundle().getPropertyValue("city");
-    private final String STREET = getBundle().getPropertyValue("street");
-    private final String STATE = getBundle().getPropertyValue("state");
-    private final String ZIP = getBundle().getPropertyValue("zip");
-    private final String PHONE_NUMBER = getBundle().getPropertyValue("phoneNumber");
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
@@ -41,7 +27,6 @@ public class POMStepDefs extends WebDriverTestCase {
         getDriver().get(getBundle().getPropertyValue("env.baseurl"));
 
     }
-    //@Test(enabled = false)
     @Given("^I login to real estate site$")
     public void loginTest() {
         LoginPage loginPage = new LoginPage();
@@ -50,39 +35,5 @@ public class POMStepDefs extends WebDriverTestCase {
         homePage.validateUserLoggedIn(USERNAME);
     }
 
-    @Test(enabled = true)
-    public void addNewRentalOwnerTest(){
-        LoginPage loginPage = new LoginPage();
-        if (!loginPage.isLoggedIn()) {
-            loginPage.logout();
-        }
-        HomePage homePage = loginPage.login(USERNAME, PASSWORD);
-        logStepStart("Navigate to Rental Owner page");
-        RentalOwnersPage rentalOwner = homePage.navigateRentalOwnersPage();
-        logStepStart("Enter new owner details");
-        rentalOwner.clearOwnerEntry(OWNER_FIRSTNAME, OWNER_LASTNAME);
-        RentalOwnerDetailsPage rentalOwnerDetailsPage = rentalOwner.clickAddNew();
-        rentalOwnerDetailsPage.validateRentalOwnerDetailsPage();
-        rentalOwnerDetailsPage.addNewOwnerEntry(OWNER_FIRSTNAME, OWNER_LASTNAME, COMPANY, YEAR, MONTH, DAY, EMAIL, PHONE_NUMBER, COUNTRY, STATE, CITY, STREET, ZIP);
-        DeviceUtils.waitForPresentTextVisual("the new record has been saved successfully",10);
-        rentalOwnerDetailsPage.returnToRentalOwnersPage();
-        logStepStart("Validate entry");
-        DeviceUtils.swipe("50%,85%","50%,25%");
-        Assert.assertTrue(rentalOwner.getValidation(OWNER_FIRSTNAME, OWNER_LASTNAME));
-    }
 
-    @Test(enabled = false)
-    public void addNewUnitTest(){
-        logStepStart("Browse to Login page");
-        LoginPage loginPage = new LoginPage();
-        HomePage homePage = loginPage.login(USERNAME, PASSWORD);
-        logStepStart("Navigate to Units page");
-        Units units = homePage.navUnits();
-        logStepStart("Enter new owner details");
-        units.clearUnit(UNIT);
-        units.addNewUnit(UNIT, MONTH);
-        if(units.validateUnit(UNIT)){
-            units.clearUnit(UNIT);
-        }
-        }
 }
